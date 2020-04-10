f9b533e4141c2c83b98bf2d1634509d1ad29df4c
iduta <ionutcosmin.duta@unitn.it>
2020-04-10 13:16:16 +0400

    Code cleanup

--- models/build_model.py
+++ models/build_model.py
@@ -49,16 +49,19 @@ def build_model(args):
 
         if args.model_depth == 18:
             model = resgroupfix.resgroupfix50(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
         elif args.model_depth == 34:
             model = resgroupfix.resgroupfix101(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
         elif args.model_depth == 50:
             model = resgroupfix.resgroupfix152(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
@@ -67,16 +70,19 @@ def build_model(args):
 
         if args.model_depth == 18:
             model = resgroup.resgroup50(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
         elif args.model_depth == 34:
             model = resgroup.resgroup101(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
         elif args.model_depth == 50:
             model = resgroup.resgroup152(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual,
                 groups=args.groups)
@@ -85,38 +91,47 @@ def build_model(args):
 
         if args.model_depth == 18:
             model = iresnet.iresnet18(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 34:
             model = iresnet.iresnet34(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 50:
             model = iresnet.iresnet50(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 101:
             model = iresnet.iresnet101(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 152:
             model = iresnet.iresnet152(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 200:
             model = iresnet.iresnet200(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 302:
             model = iresnet.iresnet302(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 404:
             model = iresnet.iresnet404(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 1001:
             model = iresnet.iresnet1001(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
 
@@ -124,26 +139,32 @@ def build_model(args):
 
         if args.model_depth == 18:
             model = resstage.resstage18(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 34:
             model = resstage.resstage34(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 50:
             model = resstage.resstage50(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 101:
             model = resstage.resstage101(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 152:
             model = resstage.resstage152(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 200:
             model = resstage.resstage200(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
 
@@ -151,26 +172,32 @@ def build_model(args):
 
         if args.model_depth == 18:
             model = resnet.resnet18(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 34:
             model = resnet.resnet34(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 50:
             model = resnet.resnet50(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 101:
             model = resnet.resnet101(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 152:
             model = resnet.resnet152(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
         elif args.model_depth == 200:
             model = resnet.resnet200(
+                pretrained=args.pretrained,
                 num_classes=args.n_classes,
                 zero_init_residual=args.zero_init_residual)
 
